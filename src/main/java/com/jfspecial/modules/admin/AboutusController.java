2b6fa98de5b3ee3eee62b3d637250ee2ab8f232a
a197628810 <45024250+a197628810@users.noreply.github.com>
2018-11-22 18:03:29 +0800

    aboutus
    后台-关于我们的java文件

---
+++ src/main/java/com/jfspecial/modules/admin/AboutusController.java
@@ -0,0 +1,71 @@
+package com.jfspecial.modules.admin.aboutus;
+
+
+import com.jfspecial.jfinal.base.BaseController;
+import com.jfspecial.jfinal.component.annotation.ControllerBind;
+import com.jfspecial.jfinal.component.db.SQLUtils;
+
+import javax.xml.ws.RequestWrapper;
+
+/**
+ * 网站配置
+ * 
+ * @author ZR2018.11.20
+ */
+@ControllerBind(controllerKey = "/admin/aboutus")
+public class AboutusController extends BaseController {
+
+	private static final String path = "/pages/admin/aboutus/wangzhanshezhi_";
+	
+	public void index() {
+
+		//System.out.println("测试1:index++++++++++++"+getPara(0));//测试方法1
+		show();
+	}
+
+	public void show(){
+		//System.out.println("测试2:show");//测试方法1
+		SysAboutus model = SysAboutus.dao.findById("1");//根据id查出网站的信息,放在model对象中
+		//System.out.println("测试2.1:"+model);
+		setAttr("model",model);
+		render(path + "c2.html");
+
+	}
+
+	//这个方法没有试验是否有效,因为没有进来
+	public void save(){
+		//renderText("声明此方法是一个action");
+
+		//获取前台页面的值
+		//System.out.println("测试3:save++++++++"+getPara("model.introduction"));//测试方法1
+		SysAboutus model = getModel(SysAboutus.class);
+		//用set方法修改model的值
+		model.setIntroduction(getPara("model.introduction"));
+		model.setId("1");
+
+		//设置修改人和修改时间
+		Integer userid= getSessionUser().getUserid();
+		String now = getNow();
+		//aboutus.put("update_id", userid);
+		//aboutus.put("update_time", now);
+		model.setUpdateid(String.valueOf(userid));
+		model.setUpdatedate(now);
+
+		//修改
+		//System.out.println("测试:"+model);//测试
+		boolean a=model.update();
+		if(a){
+			System.out.println("成功");//测试
+		}else {
+			System.out.println("失败");//测试
+		}
+		//System.out.println("测试:"+model);//测试
+
+		//保存设置,返回给前台
+		setAttr("model", model);
+		renderMessage("保存成功");
+		show();
+	}
+}
+
+
---
+++ src/main/java/com/jfspecial/modules/admin/LogoController.java
@@ -0,0 +1,78 @@
+package com.jfspecial.modules.admin.aboutus;
+
+
+import com.jfinal.upload.UploadFile;
+import com.jfspecial.jfinal.base.BaseController;
+import com.jfspecial.jfinal.component.annotation.ControllerBind;
+import com.jfspecial.jfinal.component.db.SQLUtils;
+import com.jfspecial.system.file.util.FileUploadUtils;
+
+/**
+ * 网站配置
+ * 
+ * @author ZR2018.11.20
+ */
+@ControllerBind(controllerKey = "/admin/logo")
+public class LogoController extends BaseController {
+
+	private static final String path = "/pages/admin/aboutus/wangzhanshezhi_";
+
+	public void index() {
+
+		//System.out.println("测试1:index++++++++++++"+getPara(0));//测试方法1
+		show();
+	}
+
+	public void show(){
+		//System.out.println("测试2:show");//测试方法1
+		SysAboutus model = SysAboutus.dao.findById("1");//根据id查出网站的信息,放在model对象中
+		//System.out.println("测试2.1:"+model);
+		setAttr("model",model);
+		render(path + "c1.html");
+
+	}
+
+	//参数:model.logo
+	public void save(){
+		//renderText("声明此方法是一个action");
+		SysAboutus model = getModel(SysAboutus.class);
+
+
+		//上传图片
+		//TbSite site = getBackSite();
+		//public UploadFile getFile(String parameterName//参数名称, String saveDirectory//保存路径//默认是tomacat下upload下的文件夹, Integer maxPostSize//最大传输值, String encoding//编码,可用可不用)
+		UploadFile uploadImage=null;//声明上传文件
+		try{
+			uploadImage = getFile("model.logo","logo", FileUploadUtils.UPLOAD_MAX,"utf-8");
+		}catch(Exception exception){
+			System.out.println("路径错误");
+		}
+
+
+		//修改参数
+		model.setId("1");
+		model.setLogo("\\logo\\"+uploadImage.getFileName());
+		//System.out.println("测试:"+uploadImage.getUploadPath()+"\\"+uploadImage.getFileName());
+
+
+
+		//修改人员信息
+		Integer userid= getSessionUser().getUserid();
+		String now = getNow();
+		//model.put("update_id", userid);
+		//model.put("update_time", now);
+		model.setUpdateid(String.valueOf(userid));
+		model.setUpdatedate(now);
+
+		//修改
+		model.update();
+
+		//发送给页面
+		System.out.println("测试:"+model);//测试
+		setAttr("model", model);
+		renderMessage("保存成功");
+		show();
+	}
+}
+
+
