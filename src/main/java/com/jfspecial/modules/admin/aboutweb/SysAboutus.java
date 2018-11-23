79e3147489108de9cd3a026481a982cf8fb74b29
a197628810 <45024250+a197628810@users.noreply.github.com>
2018-11-23 10:49:34 +0800

    add websetting

---
+++ src/main/java/com/jfspecial/modules/admin/aboutweb/SysAboutus.java
@@ -0,0 +1,125 @@
+package com.jfspecial.modules.admin.aboutweb;
+
+import com.jfspecial.component.base.BaseProjectModel;
+import com.jfspecial.jfinal.component.annotation.ModelBind;
+
+
+@ModelBind(table = "tb_webinfo")
+public class SysAboutus extends BaseProjectModel<SysAboutus> {
+    private static final long serialVersionUID = 1L;
+    public static final SysAboutus dao = new SysAboutus();
+
+    private String ID="id"; //网站的id,默认为1
+    private String LOGO="logo";//网站的logo地址
+    private String INTRODUCTION="introduction";//网站的介绍内容
+    private String DEPARTMENT="department";//网站机构名称
+    private String ADDRESS="address";//机构地址
+    private String TEL="tel";//联系电话
+    private String FAX="fax";//传真
+    private String WECHAT="wechat";//微信公众号
+    private String EMAIL="email";//电子邮件
+    private String UPDATEID="updateid";//修改人的id
+    private String UPDATEDATE ="updatedate";//修改日期
+
+    public SysAboutus setId(String value) {
+        set(ID, value);
+        return this;
+    }
+
+    public String getId() {
+        return get(ID);
+    }
+
+    public SysAboutus setLogo(String value){
+        set(LOGO,value);
+        return this;
+    }
+
+    public String getLogo(){
+        return get(LOGO);
+    }
+
+    public SysAboutus setIntroduction(String value){
+        set(INTRODUCTION,value);
+        return this;
+    }
+    public String getIntroduction(){
+        return get(INTRODUCTION);
+    }
+    public SysAboutus setDepartment(String value){
+        set(DEPARTMENT,value);
+        return this;
+    }
+    public String getDepartment(){
+        return get(DEPARTMENT);
+    }
+    public SysAboutus setAddress(String value){
+        set(ADDRESS,value);
+        return this;
+    }
+    public String getAddress(){
+        return get(ADDRESS);
+    }
+    public SysAboutus setTel(String value){
+        set(TEL,value);
+        return this;
+    }
+    public String getTel(){
+        return get(TEL);
+    }
+    public SysAboutus setFax(String value){
+        set(FAX,value);
+        return this;
+    }
+    public String getFax(){
+        return get(FAX);
+    }
+    public SysAboutus setWechat(String value){
+        set(WECHAT,value);
+        return this;
+    }
+    public String getWechat(){
+        return get(WECHAT);
+    }
+    public SysAboutus setEmail(String value){
+        set(EMAIL,value);
+        return this;
+    }
+    public String getEmail(){
+        return get(EMAIL);
+    }
+
+    @Override
+    public String toString() {
+        return "SysAboutus{" +
+                "ID='" + ID + '\'' +
+                ", LOGO='" + LOGO + '\'' +
+                ", INTRODUCTION='" + INTRODUCTION + '\'' +
+                ", DEPARTMENT='" + DEPARTMENT + '\'' +
+                ", ADDRESS='" + ADDRESS + '\'' +
+                ", TEL='" + TEL + '\'' +
+                ", FAX='" + FAX + '\'' +
+                ", WECHAT='" + WECHAT + '\'' +
+                ", EMAIL='" + EMAIL + '\'' +
+                ", UPDATEID='" + UPDATEID + '\'' +
+                ", UPDATEDATE='" + UPDATEDATE + '\'' +
+                '}';
+    }
+
+    public SysAboutus setUpdateid(String value){
+        set(UPDATEID,value);
+        return this;
+    }
+    public String getUpdateid(){
+        return get(UPDATEID);
+    }
+
+    public SysAboutus setUpdatedate(String value){
+        set(UPDATEDATE,value);
+        return this;
+    }
+    public String getUpdatedate(){
+        return get(UPDATEDATE);
+    }
+
+}
---
+++ src/main/java/com/jfspecial/modules/admin/aboutweb/WebInfoController.java
@@ -0,0 +1,113 @@
+package com.jfspecial.modules.admin.aboutweb;
+
+
+import com.jfinal.kit.PathKit;
+import com.jfinal.upload.UploadFile;
+import com.jfspecial.component.util.ImageModel;
+import com.jfspecial.component.util.ImageUtils;
+import com.jfspecial.jfinal.base.BaseController;
+import com.jfspecial.jfinal.component.annotation.ControllerBind;
+import com.jfspecial.jfinal.component.db.SQLUtils;
+import com.jfspecial.modules.admin.site.TbSite;
+import com.jfspecial.system.file.util.FileUploadUtils;
+import com.jfspecial.util.StrUtils;import com.jfinal.kit.PathKit;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Page;
+import com.jfinal.upload.UploadFile;
+import com.jfspecial.component.base.BaseProjectController;
+import com.jfspecial.component.util.ImageModel;
+import com.jfspecial.component.util.ImageUtils;
+import com.jfspecial.jfinal.component.annotation.ControllerBind;
+import com.jfspecial.jfinal.component.db.SQLUtils;
+import com.jfspecial.modules.admin.image.model.TbImage;
+import com.jfspecial.modules.admin.image.model.TbImageTags;
+import com.jfspecial.modules.admin.image.service.ImageAlbumService;
+import com.jfspecial.modules.admin.site.TbSite;
+import com.jfspecial.system.file.util.FileUploadUtils;
+import com.jfspecial.util.StrUtils;
+
+
+import java.io.File;
+
+/**
+ * 网站配置
+ * 
+ * @author ZR2018.11.20
+ */
+@ControllerBind(controllerKey = "/admin/introduction")
+public class WebInfoController extends BaseController {
+
+	private static final String path = "/pages/admin/aboutweb/wangzhanshezhi_";
+	
+	public void index() {
+		//System.out.println("测试1:index");//测试方法1
+		show();
+	}
+
+	public void show(){
+		//System.out.println("测试2:show");//测试方法1
+		SysAboutus model = SysAboutus.dao.findById("1");//根据id查出网站的信息,放在model对象中
+		//System.out.println("测试2.1:"+model);
+		setAttr("model",model);
+		render(path + "c3.html");
+
+	}
+
+	//这个方法没有试验是否有效,因为没有进来
+	public void save(){
+
+		//renderText("声明此方法是一个action");
+
+		//上传图片
+		//TbSite site = getBackSite();
+		//public UploadFile getFile(String parameterName//参数名称, String saveDirectory//保存路径//默认是tomacat下upload下的文件夹, Integer maxPostSize//最大传输值, String encoding//编码,可用可不用)
+		//UploadFile uploadImage = getFile("model.image_url", FileUploadUtils.getUploadTmpPath(site), FileUploadUtils.UPLOAD_MAX);
+		UploadFile uploadImage=null;
+		try{
+			uploadImage = getFile("model.wechat","logo",FileUploadUtils.UPLOAD_MAX,"utf-8");
+		}catch(Exception exception){
+			System.out.println("路径错误");
+		}
+
+
+		//获取前台页面的值
+		//System.out.println("测试3:save++++++++"+getPara("model.introduction"));//测试方法1
+		SysAboutus model = getModel(SysAboutus.class);
+		//用set方法修改model的值
+		model.setId("1");
+		model.setDepartment(getPara("model.department"));
+		model.setAddress(getPara("model.address"));
+		model.setTel(getPara("model.tel"));
+		model.setFax(getPara("model.fax"));
+		model.setEmail(getPara("model.email"));
+		model.setWechat(uploadImage.getUploadPath()+"\\"+uploadImage.getFileName());
+		//System.out.println("测试:"+uploadImage.getUploadPath()+uploadImage.getFileName());
+
+
+
+		//设置修改人和修改时间
+		Integer userid= getSessionUser().getUserid();
+		String now = getNow();
+		//aboutus.put("update_id", userid);
+		//aboutus.put("update_time", now);
+		model.setUpdateid(String.valueOf(userid));
+		model.setUpdatedate(now);
+
+		//修改
+		//System.out.println("测试:"+model);//测试
+		boolean a=model.update();
+		if(a){
+			System.out.println("成功");//测试
+		}else {
+			System.out.println("失败");//测试
+		}
+		//System.out.println("测试:"+model);//测试
+
+		//保存设置,返回给前台
+		setAttr("model", model);
+		renderMessage("保存成功");
+		show();
+	}
+}
+
+
