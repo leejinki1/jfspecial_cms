ff3126e801300d6b96e6285084c7d790c5420e24
coiggahou <coiggahou@foxmail.com>
2018-12-03 20:39:14 +0800

    add trd  module

---
+++ src/main/java/com/jfspecial/modules/front/controller/PolicyTagsController.java
@@ -0,0 +1,42 @@
+package com.jfspecial.modules.front.controller;
+
+import com.jfinal.aop.Before;
+import com.jfinal.plugin.activerecord.Page;
+import com.jfspecial.component.base.BaseProjectController;
+import com.jfspecial.jfinal.component.annotation.ControllerBind;
+import com.jfspecial.modules.admin.trd.model.TbTrd;
+import com.jfspecial.modules.front.interceptor.FrontInterceptor;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+@ControllerBind(controllerKey = "/front/policy_tags")
+public class PolicyTagsController extends BaseProjectController {
+
+	public static final String path = "/tags/";
+
+	@Before(FrontInterceptor.class)
+	public void index() {
+		String tagName = getPara();
+		try {
+			tagName = (tagName == null ? "" : tagName);
+			tagName = URLDecoder.decode(tagName, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		setAttr("tagName", tagName);
+
+		Page<TbTrd> articles = TbTrd.dao.paginate(getPaginator(), " select t.*", //
+				" from tb_trd t " //
+						+ " where (t.name like ? or t.content like ?) " //
+						+ " and t.status = 1   " // 查询状态为显示，类型是预览和正常的文章
+						+ " order by t.update_time desc ", "%" + tagName + "%", "%" + tagName + "%");
+
+		setAttr("page", articles);
+
+		renderAuto(path + "policy_search.html");
+	}
+
+
+}
